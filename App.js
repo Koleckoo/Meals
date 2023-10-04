754d7c074cd3753854505ed4dec3d8b450ae3189
jan.illetsko@greyson.eu <jan.illetsko@greyson.eu>
2023-10-04 11:30:23 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+function DrawerNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTab.Navigator
+      sceneContainerStyle={{ backgroundColor: "#3f2f25" }}
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#351401",
+          shadowOffset: { width: 0, height: 0 },
+          elevation: 0,
+        },
+        headerTintColor: "white",
+        tabBarStyle: {
+          backgroundColor: "#351401",
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: "white",
+      }}
+    >
+      <BottomTab.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons
+              name="category"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="star"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: {
+              backgroundColor: "#3f2f25",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            options={{
+              headerShown: false,
+            }}
+            component={DrawerNavigator}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId
+            //   return {
+            //     title: catId
+            //   }
+            // }}
+            component={MealsOverviewScreen}
+          />
+          <Stack.Screen
+            name="MealsDetail"
+            component={MealDetailsScreen}
+            options={{
+              title: "Meal Detail",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
